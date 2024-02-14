07c1da0f4b4aea391f94292ff9cdf7d693ce95bb
Melissa Wright <meliswright925@gmail.com>
2024-02-14 00:38:25 -0500

    updated server.js and images to public

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
+
+// Controllers 
+const routes = require('./controllers');
+
+// Helpers
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,8 +16,8 @@ const PORT = process.env.PORT || 3001;
 // Creates the Handlebars.js engine object with custom helper functions
 const hbs = exphbs.create({ helpers });
 
-// Inform Express.js which template engine we're using
-app.engine('handlebars', hbs.engine);
+// // Set Handlebars as the view engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Session middleware
@@ -27,7 +31,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Use routes
 app.use(routes);
+ 
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
