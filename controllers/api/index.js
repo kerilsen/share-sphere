2a7bcabe314340b2243fc288c7ae8df8f1e551cd
EE <evelyneidsath@gmail.com>
2024-02-19 17:53:47 -0800

    create api routes for posts and comments, add to index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,13 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const materialRoutes = require('./materialRoutes');
 const communityRoutes = require('./communityRoutes');
+const postRoutes = require('./postRoutes');
+const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
 router.use('/materials', materialRoutes);
 router.use('/communities', communityRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
