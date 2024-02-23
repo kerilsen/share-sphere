3c49ccd8cdc71fd12346c5998faca0573e8cf286
eaeidsath <evelyneidsath@gmail.com>
2024-02-23 04:45:09 -0800

    material info now displays on community page, add profile pic placeholder image

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,4 +3,26 @@ module.exports = {
       // Format date as MM/DD/YYYY
       return date.toLocaleDateString();
     },
+
+    placeholder_image: (category) => {
+      if (category === 'Camping') {
+        return '<img src="../images/landing_page/icons/camping-icon.png">'
+      } else if (category === 'Events') {
+        return '<img src="../images/landing_page/icons/event-supplies.png">'
+      } else if (category === 'Exercise') {
+        return '<img src="../images/landing_page/icons/exercise-supplies.png">'
+      } else if (category === 'Children') {
+        return '<img src="../images/landing_page/icons/children-equipment.png">'
+      } else if (category === 'Hardware') {
+        return '<img src="../images/landing_page/icons/tools-icon.png">'
+      } else if (category === 'Medical') {
+        return '<img src="../images/landing_page/icons/medical-equipment.png">'
+      } else if (category === 'Outdoor') {
+        return '<img src="../images/landing_page/icons/camping-icon.png">'
+      } else if (category === 'Discover passes') {
+        return '<img src="../images/landing_page/icons/event-game-passes.png">'
+      } else if (category === 'Games') {
+        return '<img src="../images/landing_page/icons/event-game-passes.png">'
+      }
+    }
 };
