968c5944059a3c603f1b6961b2784e52acafd481
eaeidsath <evelyneidsath@gmail.com>
2024-02-24 02:47:00 -0800

    attempt to make modal for listing item details, not working properly

--- public/js/logout.js
+++ public/js/logout.js
@@ -4,6 +4,7 @@ logoutButton.addEventListener("click", async () => {
   try {
     const response = await fetch('/api/users/logout', {
       method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
